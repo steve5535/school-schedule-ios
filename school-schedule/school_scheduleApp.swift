c21cac92322e8d1cdb9b5e62f411e20371a1fab3
성선혁 <steve5535@naver.com>
2026-01-04 00:57:53 +0900

    Initial Commit

---
+++ school-schedule/school_scheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  school_scheduleApp.swift
+//  school-schedule
+//
+//  Created by 성선혁 on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct school_scheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
